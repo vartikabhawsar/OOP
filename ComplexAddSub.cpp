fc67c005fcc0247597e3c2a07ed4a8c95f988587
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2022-11-09 23:43:09 +0530

    Add files via upload

--- ComplexAddSub.cpp
+++ ComplexAddSub.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-
 using namespace std;
 class complex
 {
@@ -7,37 +6,38 @@ class complex
     float y;//img
     public:
     void display();
-    complex(){}
-    complex (float real,float img) 
+    
+    complex (int real, int img) 
     {
         x=real;
         y=img;
     }
-    complex operator +(complex c);
+    complex operator +(complex ca);
+    complex operator -(complex cb);
 };
-complex complex ::operator +(complex c)
+complex complex::operator +(complex ca)
 {
-    complex c1;
-    c1.x=x+c.x;
-    c2.y=x+c.y;
-    return c1;
+    complex c1(0,0);  //c4    0,0 for constructor
+    c1.x=x+ca.x;
+    c1.y=y+ca.y;
+    return c1;  //c4
 }
-complex complex ::operator -(complex c)
+complex complex::operator -(complex cb)
 {
-    complex c1;
-    c1.x=x-c.x;
-    c2.y=x-c.y;
-    return c1;
+    complex c5(0,0);   //c4
+    c5.x=x-cb.x;
+    c5.y=y-cb.y;
+    return c5;      //c4
 }
-void display()
+ void complex :: display(void)
 {
-    cout<<x<<" + "<<y<<"i "<<"\n";
+    cout<< x <<" + "<< y <<"i "<<"\n";
 }
 int main()
 {
-    complex c2,c3,c4;
-    c2=complex(2,3);
-    c3=complex(5,4);
+    complex c4(0,0);  //for constructor agar contructor nai lete to input() banate
+    complex c2(2,3);
+    complex  c3(5,4);
     int a;
     cout<<"enter 1 for add and 2 for sub";
     cin>>a;
@@ -45,15 +45,24 @@ int main()
     {
         case 1:
         c4=c2+c3;
-        cout<<c2.display();
-        cout<<c3.display();
-        cout<<c4.display();
+        c2.display();
+        c3.display();
+    
+        c4.display();
+       
         break;
         case 2:
         c4=c2-c3;
-        cout<<c2.display();
-        cout<<c3.display();
-        cout<<c4.display();
+        c2.display();
+        c3.display();
+    
+        c4.display();
+       
         break;
+        default:
+        cout<<"invalid";
+
+
     }
+    
 }
