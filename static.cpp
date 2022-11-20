bc193f3c716fdb3f032f1aab40cbbc1fe5ca896b
vartikabhawsar <98348633+vartikabhawsar@users.noreply.github.com>
2022-11-20 23:17:42 +0530

    Add files via upload

--- static.cpp
+++ static.cpp
@@ -1,30 +1,54 @@
 #include <iostream>
+#include<string.h>
 using namespace std;
 class employee
 {
-    static char name[20];
-    static char desig[10];
-    static float sal;
-    
+     char name[20];
+     char desig[10];
+     float sal;
+     
+    static float s;
+    static float c;
     public:
-    static void input()
+     void input()
     {
+        cout<<"enter name, desig, sal  :  ";      
         cin>>name;
         cin>>desig;
         cin>>sal;
+        
+        if(strcmp(desig,"manager") == 0)
+        {
+         cout<<"help";   
+         s=sal+s;
+         c++;
+        }
     }
-    static void avg()
+    void output()
     {
-        float a = sal/5;
-        cout<<a;
+       cout<<strlen(desig)<<" ";
+        cout<<name<<" ";
+        cout<<desig<<" ";
+        cout<<sal<<" ";
+    }
+    static void average()
+    {
+
+        cout<<"average salary of manager is "<<  s/ c;
     }};
-   float employee:: sal=0;
-   char employee:: name[20]={};
-   char employee:: desig[10]={};
- 
+   
+    float employee ::s=0;
+    float employee ::c=0;
+
 
 int main()
 {
-    employee :: input();
-    employee::avg();
+    employee e1[3];
+    for(int i=0;i<3;i++)
+    {
+    e1[i].input();
+    e1[i].output();
+    
+}
+employee::average();
 }
